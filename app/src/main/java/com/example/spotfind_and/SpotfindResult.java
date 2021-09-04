b19e6da69a29533263bbb64c80a0ea423a3c0a87
TomasAQ <tomasaq0511@gmail.com>
2021-09-04 22:17:17 +0900

    version0.0 complet

--- app/src/main/java/com/example/spotfind_and/SpotfindResult.java
+++ app/src/main/java/com/example/spotfind_and/SpotfindResult.java
@@ -5,26 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.example.spotfind_and.privateData.NetWorkPrivateInfo;
+
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
-public class SpotfindResult extends AppCompatActivity implements View.OnClickListener {
+public class  SpotfindResult extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = SpotfindResult.class.getSimpleName();
     ImageView mImgVResultSpotimg;
     TextView mTxtVResultSpotmane , mTxtVResultLocation;
-    Button mBtnSearchYoutube;
-
+    Button mBtnSearchYoutube , mBtnSearchMap;
+    String sportTitle, sportLocation, sportImageUrl;
+    NetWorkPrivateInfo netWorkPrivateInfo = new NetWorkPrivateInfo();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spotfind_result);
         init();
 
+        Intent intent = getIntent();
+        sportTitle = intent.getExtras().getString(netWorkPrivateInfo.getRequestPrivateData1());
+        sportLocation= intent.getExtras().getString(netWorkPrivateInfo.getRequestPrivateData2());
+        sportImageUrl = intent.getExtras().getString(netWorkPrivateInfo.getRequestPrivateData3());
+
+
+        mTxtVResultSpotmane.setText("관광지명 : "+sportTitle);
+        mTxtVResultLocation.setText("위치 : "+sportLocation);
+
+        Glide.with(getApplicationContext()).load(sportImageUrl).into(mImgVResultSpotimg);
+
         mBtnSearchYoutube.setOnClickListener(this);
+        mBtnSearchMap.setOnClickListener(this);
+
     }
 
     void init(){
@@ -32,6 +51,7 @@ public class SpotfindResult extends AppCompatActivity implements View.OnClickLis
         mTxtVResultSpotmane = findViewById(R.id.txt_v_result_spotname);
         mTxtVResultLocation = findViewById(R.id.txt_v_result_location);
         mBtnSearchYoutube = findViewById(R.id.btn_search_youtube);
+        mBtnSearchMap = findViewById(R.id.btn_search_map);
     }
 
     @Override
@@ -42,10 +62,20 @@ public class SpotfindResult extends AppCompatActivity implements View.OnClickLis
                 // todo 유튜브 라이브러리를 활용해서 유튜브 앱이로 이동이 아닌 자체 앱에 유튜브 동영상 가져오기
 
                 Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("https://www.youtube.com/results?search_query=%EC%95%88%EC%84%B1%ED%8C%9C%EB%9E%9C%EB%93%9C"));
+                intent.setData(Uri.parse("https://www.youtube.com/results?search_query="+URLDecodeing(sportTitle)));
 
                 intent.setPackage("com.google.android.youtube");
                 startActivity(intent);
+                break;
+
+            case R.id.btn_search_map:
+
+//                String url = "kakaomap://open";
+                //kakaomap://open?page=placeSearch
+                String url = "kakaomap://search?q="+sportTitle;
+
+                Intent intent2 = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent2);
                 break;
         }
     }
